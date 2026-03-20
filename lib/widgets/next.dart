6b1166f6e9c64031cd14c487c2d34d00215ddea3
Dawn <merii.dunn@gmail.com>
2026-03-20 18:55:58 +0800

    feat: assets folder, images folder, setup pubspec.yaml, utils folder

---
+++ lib/widgets/next.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:siyasat_ph/theme/colors.dart';
+
+class ContinueButton extends StatelessWidget {
+  final double opacity;
+  final VoidCallback onPressed;
+  const ContinueButton({
+    super.key,
+    required this.onPressed,
+    this.opacity = 1.0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+      widthFactor: 0.9,
+      child: Opacity(
+        opacity: opacity,
+        child: TextButton(
+          onPressed: onPressed,
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.white,
+            backgroundColor: SiyasatColors.primaryTeal,
+            textStyle: TextStyle(fontSize: 16),
+          ),
+          child: Text('Next'),
+        ),
+      ),
+    );
+  }
+}
