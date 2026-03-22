383f03cf81e5a04963e5433b80f92b1c9721e62a
Dyestrm <johnmark.panganiban@wvsu.edu.ph>
2026-03-22 13:54:05 +0800

    temporary commit, bottom nav bar with animations

--- lib/widgets/bottom_nav.dart
+++ lib/widgets/bottom_nav.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import '../theme/colors.dart';
 
 
-class BottomNav extends StatelessWidget {
+class BottomNav extends StatefulWidget {
   final int currentIndex;
   final Function(int) onTap;
 
@@ -11,6 +11,46 @@ class BottomNav extends StatelessWidget {
     required this.currentIndex,
     required this.onTap,
   });
+
+  @override
+  State<BottomNav> createState() => _BottomNavState();
+}
+
+class _BottomNavState extends State<BottomNav> with TickerProviderStateMixin {
+  late AnimationController _scaleController;
+  late Animation<double> _scaleAnimation;
+  int _previousIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _scaleController = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      vsync: this,
+    );
+    _scaleAnimation = Tween<double>(
+      begin: 1.0,
+      end: 1.2,
+    ).animate(CurvedAnimation(
+      parent: _scaleController,
+      curve: Curves.elasticOut,
+    ));
+  }
+
+  @override
+  void didUpdateWidget(BottomNav oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.currentIndex != widget.currentIndex) {
+      _previousIndex = oldWidget.currentIndex;
+      _scaleController.forward(from: 0.0);
+    }
+  }
+
+  @override
+  void dispose() {
+    _scaleController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,27 +72,49 @@ class BottomNav extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: List.generate(items.length, (i) {
-          final selected = i == currentIndex;
+          final selected = i == widget.currentIndex;
+          final wasSelected = i == _previousIndex;
+
           return GestureDetector(
-            onTap: () => onTap(i),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  items[i]['icon'] as IconData,
-                  size: 24,
-                  color: selected ? AppColors.primaryTeal : AppColors.textColorGray,
-                ),
-                const SizedBox(height: 4),
-                Text(
-                  items[i]['label'] as String,
-                  style: TextStyle(
-                    fontSize: 11,
-                    color: selected ? AppColors.primaryTeal : AppColors.textColorGray,
-                    fontWeight: selected ? FontWeight.w600 : FontWeight.w400,
+            onTap: () => widget.onTap(i),
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 300),
+              curve: Curves.easeInOut,
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+              decoration: BoxDecoration(
+                color: selected ? AppColors.primaryTeal.withOpacity(0.1) : Colors.transparent,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ScaleTransition(
+                    scale: selected ? _scaleAnimation : const AlwaysStoppedAnimation(1.0),
+                    child: AnimatedContainer(
+                      duration: const Duration(milliseconds: 300),
+                      curve: Curves.easeInOut,
+                      child: Icon(
+                        items[i]['icon'] as IconData,
+                        size: selected ? 28 : 24,
+                        color: selected ? AppColors.primaryTeal : AppColors.textColorGray,
+                      ),
+                    ),
                   ),
-                ),
-              ],
+                  const SizedBox(height: 4),
+                  AnimatedDefaultTextStyle(
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.easeInOut,
+                    style: TextStyle(
+                      fontSize: selected ? 12 : 11,
+                      color: selected ? AppColors.primaryTeal : AppColors.textColorGray,
+                      fontWeight: selected ? FontWeight.w700 : FontWeight.w400,
+                    ),
+                    child: Text(
+                      items[i]['label'] as String,
+                    ),
+                  ),
+                ],
+              ),
             ),
           );
         }),
