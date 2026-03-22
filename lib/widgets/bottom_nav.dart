1269b532c7ee6902bf42032c009615cd80e6b412
Dyestrm <johnmark.panganiban@wvsu.edu.ph>
2026-03-22 14:00:37 +0800

    refactor(widgets): Restore bottom navigation with animation

--- lib/widgets/bottom_nav.dart
+++ lib/widgets/bottom_nav.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import '../theme/colors.dart';
 
 
-class BottomNav extends StatefulWidget {
+class BottomNav extends StatelessWidget {
   final int currentIndex;
   final Function(int) onTap;
 
@@ -13,112 +13,27 @@ class BottomNav extends StatefulWidget {
   });
 
   @override
-  State<BottomNav> createState() => _BottomNavState();
-}
-
-class _BottomNavState extends State<BottomNav> with TickerProviderStateMixin {
-  late AnimationController _scaleController;
-  late Animation<double> _scaleAnimation;
-  int _previousIndex = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    _scaleController = AnimationController(
-      duration: const Duration(milliseconds: 300),
-      vsync: this,
-    );
-    _scaleAnimation = Tween<double>(
-      begin: 1.0,
-      end: 1.2,
-    ).animate(CurvedAnimation(
-      parent: _scaleController,
-      curve: Curves.elasticOut,
-    ));
-  }
-
-  @override
-  void didUpdateWidget(BottomNav oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    if (oldWidget.currentIndex != widget.currentIndex) {
-      _previousIndex = oldWidget.currentIndex;
-      _scaleController.forward(from: 0.0);
-    }
-  }
-
-  @override
-  void dispose() {
-    _scaleController.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final items = [
-      {'icon': Icons.home_outlined, 'label': 'Suriin'},
-      {'icon': Icons.find_in_page, 'label': 'Kasaysayan'},
-      {'icon': Icons.tune, 'label': 'Setup'},
-      {'icon': Icons.settings_outlined, 'label': 'Settings'},
-    ];
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: onTap,
+      selectedItemColor: AppColors.primaryTeal,
+      unselectedItemColor: AppColors.textColorGray,
 
-    return Container(
-      height: 72,
-      decoration: const BoxDecoration(
-        color: Colors.white,
-        border: Border(
-          top: BorderSide(color: AppColors.primaryTeal, width: 1),
-        ),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: List.generate(items.length, (i) {
-          final selected = i == widget.currentIndex;
-          final wasSelected = i == _previousIndex;
+      items: const [
+        BottomNavigationBarItem( icon:Icon(Icons.home),
+          label: 'Suriin' ),
 
-          return GestureDetector(
-            onTap: () => widget.onTap(i),
-            child: AnimatedContainer(
-              duration: const Duration(milliseconds: 300),
-              curve: Curves.easeInOut,
-              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-              decoration: BoxDecoration(
-                color: selected ? AppColors.primaryTeal.withOpacity(0.1) : Colors.transparent,
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ScaleTransition(
-                    scale: selected ? _scaleAnimation : const AlwaysStoppedAnimation(1.0),
-                    child: AnimatedContainer(
-                      duration: const Duration(milliseconds: 300),
-                      curve: Curves.easeInOut,
-                      child: Icon(
-                        items[i]['icon'] as IconData,
-                        size: selected ? 28 : 24,
-                        color: selected ? AppColors.primaryTeal : AppColors.textColorGray,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 4),
-                  AnimatedDefaultTextStyle(
-                    duration: const Duration(milliseconds: 300),
-                    curve: Curves.easeInOut,
-                    style: TextStyle(
-                      fontSize: selected ? 12 : 11,
-                      color: selected ? AppColors.primaryTeal : AppColors.textColorGray,
-                      fontWeight: selected ? FontWeight.w700 : FontWeight.w400,
-                    ),
-                    child: Text(
-                      items[i]['label'] as String,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          );
-        }),
-      ),
+        BottomNavigationBarItem(icon: Icon(Icons.find_in_page),
+          label: 'Kasaysayan' ),
+
+        BottomNavigationBarItem(icon: Icon(Icons.reorder),
+          label: 'Setup' ),
+
+        BottomNavigationBarItem(icon: Icon(Icons.settings),
+          label: 'Settings' ),
+      ]
     );
+
   }
 }
